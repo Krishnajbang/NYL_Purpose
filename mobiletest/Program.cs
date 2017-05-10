f4c53d15ff45b554d255aaae102cbc938deb2f78
Krish <Krish@ASUS>
2017-05-10 23:18:57 +0530

    Add project files.

---
+++ mobiletest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support;
+using System.Threading;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.ii
+
+
+
+
+
+namespace mobiletest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
